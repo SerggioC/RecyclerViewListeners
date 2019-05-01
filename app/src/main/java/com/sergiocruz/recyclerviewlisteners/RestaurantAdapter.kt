d0a04b05f836cf4df9952ae6790c98cda63e725a
Sergio <sergiocrz@hotmail.com>
2019-05-02 00:26:15 +0100

    magic sorting animations

--- app/src/main/java/com/sergiocruz/recyclerviewlisteners/RestaurantAdapter.kt
+++ app/src/main/java/com/sergiocruz/recyclerviewlisteners/RestaurantAdapter.kt
@@ -1,16 +1,24 @@
 package com.sergiocruz.recyclerviewlisteners
 
+import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AnticipateOvershootInterpolator
 import android.widget.LinearLayout
+import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
-import com.sergiocruz.recyclerviewlisteners.MainActivity.*
+import com.sergiocruz.recyclerviewlisteners.MainActivity.Field
 import com.sergiocruz.recyclerviewlisteners.MainActivity.Field.*
+import com.sergiocruz.recyclerviewlisteners.MainActivity.SortOrder
 import kotlinx.android.synthetic.main.item_restaurant.view.*
+import java.util.*
+import kotlin.Comparator
+import kotlin.collections.LinkedHashMap
 
 
 class RestaurantAdapter(
@@ -18,65 +26,89 @@ class RestaurantAdapter(
     private val sorter: (view: View?, position: Int?) -> Unit,
     private val inflater: LayoutInflater
 ) :
-    RecyclerView.Adapter<RestaurantAdapter.RestauranteViewHolder>() {
+    RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder>() {
 
     init {
         setHasStableIds(true)
     }
 
-    override fun getItemCount(): Int {
-        return data?.size ?: 0
-    }
+    override fun getItemCount() = data?.size ?: 0
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestauranteViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder {
         val restaurantView = inflater.inflate(R.layout.item_restaurant, parent, false)
-        return RestauranteViewHolder(restaurantView)
+        return RestaurantViewHolder(restaurantView)
     }
 
-    override fun onBindViewHolder(holder: RestauranteViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
         val mesa: Mesa? = data?.get(position)
         val productList: MutableList<Product> = mesa?.productList ?: return
 
         val linearLayout = holder.linearLayout
         drawRows(linearLayout, productList)
-
     }
 
     private fun drawRows(linearLayout: LinearLayout?, productList: MutableList<Product>?) {
-        if (productList == null) return
-
-        linearLayout?.removeAllViews()
-
+        if (productList == null || linearLayout == null) return
+        linearLayout.removeAllViews()
         for (product in productList) {
             val row: View = inflater.inflate(R.layout.item_product_mesa, null, false)
             val productHolder = bindProductRow(row, product)
-            animateRow(productHolder)
-            linearLayout?.addView(productHolder)
+            productHolder.rotateX()
+            linearLayout.addView(productHolder)
         }
     }
 
-    private fun animateRow(view: View) {
-        val rotateX = ObjectAnimator.ofFloat(view, "rotationX", 180f, 360f)
-        rotateX.interpolator = AccelerateDecelerateInterpolator()
 
-        val alpha = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f)
+    private fun getViewsOrder(
+        linearLayout: LinearLayout?,
+        adapterPosition: Int
+    ): LinkedHashMap<Product?, View?> {
+        val linkedViewsToPosition: LinkedHashMap<Product?, View?> =
+            LinkedHashMap()
 
-        val animationSet = AnimatorSet()
-        animationSet.duration = 400
-        animationSet.playTogether(rotateX, alpha)
-        animationSet.start()
+        sortViewsByY(linearLayout)
+            ?.asIterable()
+            ?.withIndex()
+            ?.forEach { it: IndexedValue<Map.Entry<Float, View?>> ->
+                val view: View? = it.value.value
+                val product: Product? = data?.get(adapterPosition)?.productList?.get(it.index)
+                linkedViewsToPosition[product] = view
+            }
+
+        return linkedViewsToPosition
+    }
+
+    private fun sortViewsByY(linearLayout: LinearLayout?): SortedMap<Float, View?>? {
+        val viewY: LinkedHashMap<Float, View?>? = LinkedHashMap()
+        linearLayout?.children?.forEach { childView ->
+            viewY?.set(childView.y, childView)
+        }
+        val viewYSorted: SortedMap<Float, View?>? =
+            viewY?.toSortedMap(Comparator { f1, f2 -> compareValues(f1, f2) })
+        return viewYSorted
     }
 
 
-    private fun sortThisByField(
-        position: Int,
-        field: Field
-    ): MutableList<Product>? {
-        val mesa = data?.get(position)
+    private fun getViewAtIndexByY(linearLayout: LinearLayout?, index: Int): View? {
+        sortViewsByY(linearLayout)
+            ?.asIterable()
+            ?.withIndex()
+            ?.forEach { it: IndexedValue<Map.Entry<Float, View?>> ->
+                if (it.index == index)
+                    return it.value.value
+            }
+        return null
+    }
+
+    private fun sortThisByField(linearLayout: LinearLayout?, adapterPosition: Int, field: Field) {
+        if (isAnimating) return
+        val previousViewsOrders: LinkedHashMap<Product?, View?> =
+            getViewsOrder(linearLayout, adapterPosition)
+
+        val mesa = data?.get(adapterPosition)
         val productsOfMesaX = mesa?.productList
         val order: HashMap<HashMap<Int?, Field>, SortOrder>? = mesa?.sortOrder
         val sortOrder: SortOrder = order?.get(hashMapOf(mesa.id to field)) ?: SortOrder.ORIGINAL
-
         when (sortOrder) {
             SortOrder.ASCENDING -> {
                 productsOfMesaX?.sortWith(Comparator { p1, p2 -> comparator(field, p2, p1) })
@@ -87,8 +119,68 @@ class RestaurantAdapter(
                 mesa?.sortOrder?.set(hashMapOf(mesa.id to field), SortOrder.ASCENDING)
             }
         }
-        return mesa?.productList
+        animateViewsToNewOrder(linearLayout, mesa?.productList, previousViewsOrders)
     }
+
+    private fun animateViewsToNewOrder(
+        linearLayout: LinearLayout?,
+        newProductOrder: MutableList<Product>?,
+        previousViewsOrders: LinkedHashMap<Product?, View?>
+    ) {
+        val animationList = mutableListOf<Animator>()
+        newProductOrder?.withIndex()?.forEach { (newIndex: Int, product: Product) ->
+            val previousView: View? = previousViewsOrders[product]
+            val newView = getViewAtIndexByY(linearLayout, newIndex)
+
+            val animY = ObjectAnimator.ofFloat(previousView, "y", newView?.y ?: 0f)
+            val rotateX = ObjectAnimator.ofFloat(previousView, "rotationX", -280f, -360f)
+            val alpha = ObjectAnimator.ofFloat(previousView, "alpha", 0.75f, 1f)
+            val direction = if (previousView?.y ?: 0f > newView?.y ?: 0f) 1 else -1
+            val animX = ObjectAnimator.ofFloat(previousView, "x",
+                0f,
+                20f * direction,
+                0f)
+
+            val animScaleX = ObjectAnimator.ofFloat(previousView, "scaleX", 1f, 2f, 1f)
+            val animScaleY = ObjectAnimator.ofFloat(previousView, "scaleY", 1f, 2f, 1f)
+            val animElevation = ObjectAnimator.ofFloat(previousView, "elevation", 1f, 20f, 1f)
+
+//            animationList.add(animScaleX)
+//            animationList.add(animScaleY)
+//            animationList.add(animElevation)
+            animationList.add(animY)
+            animationList.add(rotateX)
+            animationList.add(alpha)
+        }
+
+        AnimatorSet().apply {
+            duration = 500
+            playTogether(animationList)
+            interpolator = AccelerateDecelerateInterpolator()
+            addListener(object : Animator.AnimatorListener {
+                override fun onAnimationRepeat(animation: Animator?) {
+                    isAnimating = true
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    isAnimating = false
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                    isAnimating = false
+                }
+
+                override fun onAnimationStart(animation: Animator?) {
+                    isAnimating = true
+                }
+            })
+            start()
+        }
+
+
+    }
+
+    var isAnimating = false
 
     private fun comparator(field: Field, p1: Product?, p2: Product?): Int {
         return when (field) {
@@ -99,27 +191,25 @@ class RestaurantAdapter(
         }
     }
 
-    inner class RestauranteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class RestaurantViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val linearLayout: LinearLayout? = view.content
+
         init {
+
             view.theId.setOnClickListener {
-                val productList = sortThisByField(adapterPosition, Field1)
-                drawRows(linearLayout, productList)
+                sortThisByField(linearLayout, adapterPosition, Field1)
             }
 
             view.nome.setOnClickListener {
-                val productList = sortThisByField(adapterPosition, Field2)
-                drawRows(linearLayout, productList)
+                sortThisByField(linearLayout, adapterPosition, Field2)
             }
 
             view.quantidade.setOnClickListener {
-                val productList = sortThisByField(adapterPosition, Field3)
-                drawRows(linearLayout, productList)
+                sortThisByField(linearLayout, adapterPosition, Field3)
             }
 
             view.price.setOnClickListener {
-                val productList = sortThisByField(adapterPosition, Field4)
-                drawRows(linearLayout, productList)
+                sortThisByField(linearLayout, adapterPosition, Field4)
             }
         }
     }
@@ -137,5 +227,18 @@ class RestaurantAdapter(
 
     override fun getItemViewType(position: Int) = position
 
+}
+
+private fun View.rotateX() {
+    val rotateX = ObjectAnimator.ofFloat(this, "rotationX", 200f, 360f)
+    val alpha = ObjectAnimator.ofFloat(this, "alpha", 0.4f, 1f)
+
+    AnimatorSet().apply {
+        duration = 300
+//        interpolator = AccelerateDecelerateInterpolator()
+//        interpolator = AnticipateOvershootInterpolator()
+        playTogether(rotateX, alpha)
+        start()
+    }
 }
 
